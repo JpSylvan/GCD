f47046580b59a92675ead322c0127ce02da2321a
WinnerTaco <jpsylvan@gmail.com>
2014-08-23 03:15:15 +0200

    update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -21,9 +21,10 @@ data_test = cbind(X_test, subject_test, y_test)
 data = rbind(data_train,data_test)
 
 # Keep only mean() and std() (grepl(pattern,x) tells us if pattern is included in the string x)
-keep = grepl("mean()",features) | grepl("std()",features)
-keep = c(keep,T,T)
-data = data[,keep]
+# We need to remove meanFreq() since grepl("mean()",features) is equal to TRUE when the feature name contains meanFreq() (which we
+# don't want')
+keep = (grepl("mean()",features) | grepl("std()",features)) & !grepl("meanFreq()",features)
+data = data[,c(keep,T,T)] # We add 2 TRUE values to keep, to keep the last 2 columns of data which are 
 
 # Label the activities (1=WALKING, 2=WALKING_UPSTAIRS, etc...)
 # Remove the first column of data which is activity_code (from 1 to 6) and that we don't need anymore
@@ -46,6 +47,10 @@ for (i in 1:length(datasplit)) {
 # Transform the matrix into a data.frame and add the name of the couple (subject,activity) to each row
 tidydata = data.frame(tidydata)
 tidydata = data.frame(group = names(datasplit),tidydata)
+
+#Don't know why but after the 2 previous lines, characters "-" and "()" in features names are replaced with 
+# ".", so I give the original names back
+names(tidydata) = c("subject-activity",as.character(features[keep]))
 
 #Write data in .txt files
 write.table(tidydata,file="tidydata.txt",row.name=F)
